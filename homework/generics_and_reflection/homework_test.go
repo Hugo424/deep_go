727e2d56c6ceb90fb106e6b052a13991f2d8a0fe
panasenko.daniil3 <panasenko.daniil3@wildberries.work>
2025-05-23 12:35:39 +0300

    hw 9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,29 +18,55 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+type Animal struct {
+	Name    string `properties:"name"`
+	Age     int    `properties:"age"`
+	Species string `properties:"species,omitempty"`
+}
+
+func Serialize[T any](object T) string {
+	v := reflect.ValueOf(object)
+	t := reflect.TypeOf(object)
+
+	numFields := t.NumField()
+	builder := &strings.Builder{}
+	for i := 0; i < numFields; i++ {
+		tag := t.Field(i).Tag
+		field := v.Field(i)
+
+		tagValue, tagPresent := tag.Lookup("properties")
+		isOmitempty := strings.Contains(tagValue, "omitempty")
+		tagValue = strings.Replace(tagValue, ",omitempty", "", 1)
+
+		if (!tagPresent) || (field.IsZero() && isOmitempty) {
+			continue
+		}
+
+		builder.WriteString(fmt.Sprintf("%s=%v\n", tagValue, field))
+	}
+
+	return strings.TrimSpace(builder.String())
 }
 
 func TestSerialization(t *testing.T) {
 	tests := map[string]struct {
-		person Person
+		item   any
 		result string
 	}{
-		"test case with empty fields": {
+		"test case with empty fields struct 1": {
+			item:   Person{},
 			result: "name=\nage=0\nmarried=false",
 		},
 		"test case with fields": {
-			person: Person{
+			item: Person{
 				Name:    "John Doe",
 				Age:     30,
 				Married: true,
 			},
 			result: "name=John Doe\nage=30\nmarried=true",
 		},
-		"test case with omitempty field": {
-			person: Person{
+		"test case with omitempty field struct 1": {
+			item: Person{
 				Name:    "John Doe",
 				Age:     30,
 				Married: true,
@@ -45,12 +74,32 @@ func TestSerialization(t *testing.T) {
 			},
 			result: "name=John Doe\naddress=Paris\nage=30\nmarried=true",
 		},
+		"test case with empty fields struct 2": {
+			item:   Animal{},
+			result: "name=\nage=0",
+		},
+		"test case with fields struct 2": {
+			item: Animal{
+				Name: "Barsique",
+				Age:  1,
+			},
+			result: "name=Barsique\nage=1",
+		},
+		"test case with omitempty field struct 2": {
+			item: Animal{
+				Name:    "Sharique",
+				Age:     2,
+				Species: "dog",
+			},
+			result: "name=Sharique\nage=2\nspecies=dog",
+		},
 	}
 
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
-			result := Serialize(test.person)
+			result := Serialize(test.item)
 			assert.Equal(t, test.result, result)
 		})
 	}
+
 }
